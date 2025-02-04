f7825747e0098881e5f6da3d642886aceb1b6bc4
ZIN <blue.ink.150@icloud.com>
2025-02-04 12:26:32 +0900

    FIXED: Conflict is solved.

--- main.py
+++ main.py
@@ -1,13 +1,4 @@
 from GUI import GUI
 
-<<<<<<< HEAD
-def main():
-    compressor = Compressor()
-    GUI().run(compressor)
-
-if __name__ == "__main__":
-    main()
-=======
 if __name__ == "__main__":
     GUI().run()
->>>>>>> branch-A-recovery
